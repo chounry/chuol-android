521397bc94b3f7ecf362a5dc8d472ca89eb4188d
mavin19 <koung.mavin@gmail.com>
2019-05-02 13:30:05 +0700

    new update

--- app/src/main/java/com/group6/choul/MainActivity.java
+++ app/src/main/java/com/group6/choul/MainActivity.java
@@ -1,30 +1,48 @@
 package com.group6.choul;
 
+import android.app.Dialog;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayoutHome;
     private NavigationView navigationViewHome;
     private ActionBarDrawerToggle toggle;
+    Dialog getMydialog;
+    Button btnhouse, btnroom;
+    ViewPager viewPager;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getMydialog = new Dialog(this);
         drawerLayoutHome = findViewById(R.id.drawer_home);
         navigationViewHome = findViewById(R.id.nav_home);
+//        viewPager = (ViewPager)findViewById(R.id.slide);
+//        slideadapter sladapter = new slideadapter(this);
+//        Log.e("Something ", viewPager+"");
+//        viewPager.setAdapter(sladapter);
         toggle = new ActionBarDrawerToggle(this,drawerLayoutHome,R.string.opened_menu,R.string.closed_menu);
         toggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -67,6 +85,14 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         if (toggle.onOptionsItemSelected(item))
             return true;
+
+
+        switch (item.getItemId()){
+            case R.id.post:
+                showPopup();
+                return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
     private void loadfragment(Fragment fragment){
@@ -80,6 +106,15 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.post,menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    public void showPopup() {
+        getMydialog.setContentView(R.layout.custompostpopup);
+        btnhouse=getMydialog.findViewById(R.id.btn_house);
+        btnroom=getMydialog.findViewById(R.id.btn_room);
+
+        getMydialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        getMydialog.show();
     }
 }
 
--- app/src/main/java/com/group6/choul/fragment2.java
+++ app/src/main/java/com/group6/choul/fragment2.java
@@ -4,15 +4,22 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class fragment2 extends Fragment {
+    ViewPager viewPager;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragement2,container,false);
+        View v = inflater.inflate(R.layout.housedetail,container,false);
+        viewPager = v.findViewById(R.id.slide);
+        slideadapter sladapter = new slideadapter(v.getContext());
+        Log.e("Something ", viewPager+"");
+        viewPager.setAdapter(sladapter);
         return v;
     }
 }
